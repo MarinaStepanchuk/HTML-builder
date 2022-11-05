3fc4837f728f09a4a5f4f27fd77bdb20392bc3d2
MarinaStepanchuk <106726117+MarinaStepanchuk@users.noreply.github.com>
2022-11-05 23:48:33 +0300

    feat: task 04-copy-directory complete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
 
+const fs = require('fs');
+const path = require('path');
+
+const folder = path.join(__dirname, 'files');
+const folderNew = path.join(__dirname, 'files-copy');
+
+fs.mkdir(folderNew, { recursive: true }, err => {
+    if(err) throw err;
+});
+
+fs.readdir(folder, (err, files) => {
+    if(err) throw err;
+
+    fs.readdir(folderNew, { withFileTypes: true }, (err, filesNewFolder) => {
+        if(err) throw err;
+        filesNewFolder.forEach(elem => {
+            if(!files.includes(elem.name)) {
+                fs.unlink(`${folderNew}/${elem.name}`, err => {
+                    return
+                })
+            }
+        })
+    })
+
+    files.forEach(file => {
+        fs.copyFile(`${folder}/${file}`, `${folderNew}/${file}`, err => {
+            if(err) throw err;
+        })
+    })
+});
