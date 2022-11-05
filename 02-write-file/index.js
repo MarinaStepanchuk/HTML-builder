7b2791f04fa7fa751f63df9f3911bfc7c2464a25
MarinaStepanchuk <106726117+MarinaStepanchuk@users.noreply.github.com>
2022-11-05 16:57:03 +0300

    feat: task 02-write-file complete

---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const { stdin, stdout } = process;
+const fs = require("fs");
+const path = require("path");
+
+const file = path.join(__dirname, 'text.txt');
+
+fs.writeFile(file, '', err => {
+    if(err) throw err
+});
+
+stdout.write('Добрый день. Введите текст для записи в файл:\n');
+
+stdin.on('data', data => {
+    // console.log(data.toString())
+    if (data.toString().trim() === 'exit') {
+        exitFromProcess();
+    }
+    let text = '';
+    fs.appendFile(file, data.toString(), err => {
+        if(err) throw err
+    });
+});
+
+process.on('SIGINT', exitFromProcess);
+
+function exitFromProcess() {
+    stdout.write(`Ввод завершен. Введеный текст отразится в файле ${file}`);
+    process.exit();
+}
