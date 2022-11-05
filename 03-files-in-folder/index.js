ba2842056795f1d4caa75bfcdaa2ec9db4fdbd9c
MarinaStepanchuk <106726117+MarinaStepanchuk@users.noreply.github.com>
2022-11-05 18:32:11 +0300

    feat: task 03-files-in-folder complete

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+    files.forEach(file => {
+        if (file.isFile()) {
+            fs.stat(path.join(__dirname, 'secret-folder', file.name), (err, fileObj) => {
+                console.log(file.name.split('.')[0] + ' - ' + path.extname(file.name).slice(1) + ' - ' + fileObj.size/1000 + 'kb');
+            })
+        }
+    })
+})
