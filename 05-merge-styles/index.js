63f3808df0c104090cdd91cbb58b41c3b714bbeb
MarinaStepanchuk <106726117+MarinaStepanchuk@users.noreply.github.com>
2022-11-06 20:40:28 +0300

    feat: task 05-merge-styles complete

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const bundleFile = path.join(__dirname, 'project-dist', 'bundle.css');
+const folderStyles = path.join(__dirname, 'styles');
+
+fs.writeFile(bundleFile, "", err => {
+    if (err) {
+        throw err;
+    };
+});
+
+fs.readdir(folderStyles, { withFileTypes: true }, (err, files) => {
+    if(err) throw err;
+    files.forEach(file => {
+        if(file.isFile() && path.extname(file.name).slice(1) === 'css') {
+            fs.readFile(path.join(folderStyles, file.name), 'utf8', function(error, fileContent){
+                if(error) throw error;
+                fs.appendFile(bundleFile, fileContent, err => {
+                    if(err) throw err;
+                });
+            }); 
+        };
+    });
+});
